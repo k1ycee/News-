94c738a7b7d8a5139e40bac63308014b266628fa
Denzel <chiagozieani145@gmail.com>
2020-06-10 02:01:10 +0100

    getting

--- lib/api/api_call.dart
+++ lib/api/api_call.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:http/http.dart' as caller;
+import 'package:povider/model/business_model.dart';
 import 'dart:convert';
 
 import 'package:povider/model/hng_model.dart';
@@ -9,14 +10,25 @@ import 'package:povider/model/hng_model.dart';
 
 class NewsCall{
 
-  final String url = 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0033eec1ce834b8f93dbd041496980c9';
+  
 
 
     Future<List<Article>> news() async{
+      final String url = 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0033eec1ce834b8f93dbd041496980c9';
       var getPeople = await caller.get(url);
       if(getPeople.statusCode == 200){
        return News.fromJson(jsonDecode(getPeople.body)).articles;
       }
     return null;
   }
+
+  Future<List<BusinessArticle>> business() async{
+    final String url = 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0033eec1ce834b8f93dbd041496980c9';
+      var getPeople = await caller.get(url);
+      if(getPeople.statusCode == 200){
+       return BusinessNews.fromJson(jsonDecode(getPeople.body)).articles;
+      }
+    return null;
+  }
+
 }
