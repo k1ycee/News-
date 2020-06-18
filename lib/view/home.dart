92b83f92f4b0f6f39ec4d1d48b5cfb407f4297f8
Denzel <chiagozieani145@gmail.com>
2020-06-18 13:56:30 +0100

    Listview is now wrapping the column please state if it works for you

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -47,100 +47,104 @@ class HomeScreen extends StatelessWidget {
             )
           ],
         ),
-        body: Column(
+        body: ListView(
           children: <Widget>[
-            SizedBox(
-              height: 40.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    "Tech Crunch",
-                    style: GoogleFonts.nunito(
-                        textStyle: TextStyle(
-                      color: textDarker,
-                      fontSize: 25,
-                      fontWeight: FontWeight.w800,
-                    )),
-                  ),
-                  Container(
-                    height: 32,
-                    padding: EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(6),
-                        color: background),
-                    child: Center(
-                        child: Text(
-                      "Show all",
-                      style: GoogleFonts.nunito(
-                        textStyle: TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.w600,
-                          color: text,
-                        ),
-                      ),
-                    )),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Container(
-              height: 300,
-              width: MediaQuery.of(context).size.width,
-              child: model.hng.length == 0 ? Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation(textDarker),)) :  ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: model.hng.length,
-                itemBuilder: (context, index) => TopNewsWidget(
-                  model.hng[index],
-                  index,
-                  model.hng.length,
+            Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 40.0,
                 ),
-              ),
-            ),
-            SizedBox(height: 20,),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    "Business",
-                    style: GoogleFonts.nunito(
-                        textStyle: TextStyle(
-                      color: textDarker,
-                      fontSize: 25,
-                      fontWeight: FontWeight.w800,
-                    )),
-                  ),
-                  Container(
-                    height: 32,
-                    padding: EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(6),
-                        color: background),
-                    child: Center(
-                        child: Text(
-                      "Show all",
-                      style: GoogleFonts.nunito(
-                        textStyle: TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.w600,
-                          color: text,
-                        ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Tech Crunch",
+                        style: GoogleFonts.nunito(
+                            textStyle: TextStyle(
+                          color: textDarker,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w800,
+                        )),
                       ),
-                    )),
-                  )
-                ],
-              ),
+                      Container(
+                        height: 32,
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            color: background),
+                        child: Center(
+                            child: Text(
+                          "Show all",
+                          style: GoogleFonts.nunito(
+                            textStyle: TextStyle(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w600,
+                              color: text,
+                            ),
+                          ),
+                        )),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  child: model.hng.length == 0 ? Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation(textDarker),)) :  ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: model.hng.length,
+                    itemBuilder: (context, index) => TopNewsWidget(
+                      model.hng[index],
+                      index,
+                      model.hng.length,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Business",
+                        style: GoogleFonts.nunito(
+                            textStyle: TextStyle(
+                          color: textDarker,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w800,
+                        )),
+                      ),
+                      Container(
+                        height: 32,
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            color: background),
+                        child: Center(
+                            child: Text(
+                          "Show all",
+                          style: GoogleFonts.nunito(
+                            textStyle: TextStyle(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w600,
+                              color: text,
+                            ),
+                          ),
+                        )),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 4.0,),
+                Business()
+              ],
             ),
-            SizedBox(height: 4.0,),
-            Business()
           ],
         ),
       ),
